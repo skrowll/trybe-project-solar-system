5cf4c007bd77e766bacd03209d73e226e75220f0
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-02-19 18:16:28 -0300

    Requisito 8 OK

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        <p>Missões</p>
+        <Title headline="Missões" />
       </div>
     );
   }
