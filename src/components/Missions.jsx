251f0e54bfdd979daebc5e1891d63046aca24421
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-02-20 00:45:01 -0300

    Requisito 9 e 10 OK tinha esquecido o commit

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => (
+          <div key={ missions.name }>
+            <MissionCard
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          </div>
+        ))}
       </div>
     );
   }
